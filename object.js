342b5f82232aca66fdf937787951429cc8c9f436
Your Name <you@example.com>
2025-10-20 08:55:32 +0300

    Added object.js

---
+++ object.js
@@ -0,0 +1,12 @@
+let auto = {
+    marka: "Audi",
+    modelis: "A4",
+    gads: 2004
+};
+
+console.log("Auto marka:", auto.marka);
+
+if(2025 - auto.gads > 10)
+    console.log("Auto ir vecāks par 10 gadiem.")
+else
+    console.log("Auto nav vecāks par 10 gadiem.")
